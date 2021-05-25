57051e8d8b68a1aaa46aa4c6285a18e6650339b7
SimeraHailu <68601178+SimeraHailu@users.noreply.github.com>
2021-05-25 11:19:06 -0500

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,7 @@ using Npgsql;
 using NpgsqlTypes;
 using Quartz.Impl.AdoJobStore.Common;
 using System;
+using Newtonsoft.Json;
 
 namespace Epa.Camd.Easey.JobScheduler
 {
@@ -27,15 +28,39 @@ namespace Epa.Camd.Easey.JobScheduler
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            int port = 5432;
+            int.TryParse(Configuration["EASEY_DB_PORT"], out port);
+
+            string host = Configuration["EASEY_DB_HOST"] ?? "database";
+            string user = Configuration["EASEY_DB_USER"] ?? "postgres";
+            string password = Configuration["EASEY_DB_PWD"] ?? "password";
+            string db = Configuration["EASEY_DB_NAME"] ?? "postgres";
+            string vcapServices = Configuration["VCAP_SERVICES"];
+
+            if (!string.IsNullOrWhiteSpace(vcapServices))
+            {
+                dynamic vcapSvc = JsonConvert.DeserializeObject(vcapServices);
+                dynamic vcapSvcCreds = vcapSvc["aws-rds"][0].credentials;
+
+                host = vcapSvcCreds.host;
+                port = vcapSvcCreds.port;
+                user = vcapSvcCreds.username;
+                password = vcapSvcCreds.password;
+                db = vcapSvcCreds.name;
+            }
+
+            string connectionString = $"server={host};port={port};user id={user};password={password};database={db};pooling=true";
             //services.AddScoped<NpgSqlContext>();
 
+
+
+
             services.AddDbContext<NpgSqlContext>(options =>
-                options.UseNpgsql(Configuration.GetConnectionString("Epa.Camd.Postgres"))
+                options.UseNpgsql(connectionString)
             );
 
-            string k = Configuration.GetConnectionString("Epa.Camd.Postgres");
-
-            Console.WriteLine(k);
+          
+            //DbconfigManager
 
             // base configuration from appsettings.json
             services.Configure<QuartzOptions>(Configuration.GetSection("Quartz"));
@@ -83,7 +108,7 @@ namespace Epa.Camd.Easey.JobScheduler
                 q.SetProperty("quartz.jobStore.type", "Quartz.Impl.AdoJobStore.JobStoreTX, Quartz");
                 q.SetProperty("quartz.jobStore.driverDelegateType", "Quartz.Impl.AdoJobStore.StdAdoDelegate, Quartz");
                 q.SetProperty("quartz.jobStore.dataSource", "default");
-                q.SetProperty("quartz.dataSource.default.connectionString", "server=localhost;port=15210;user id=uImcwuf4K9dyaxeL;password=f7GTHc5O3Tvy8lp9njrG3BcLU;database=cgawsbrokerprodr97macy19l;pooling=true");
+                q.SetProperty("quartz.dataSource.default.connectionString", connectionString);
                 q.SetProperty("quartz.dataSource.default.provider", "Npgsql-30");
                 q.SetProperty("quartz.jobStore.tablePrefix", "camdaux.qrtz_");
 
